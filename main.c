11f9a0badd53fc8e0e83be34437eccbc72a96200
brmatti <126803814+brmatti@users.noreply.github.com>
2023-03-02 18:38:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,26 @@
 FSA create_fsa(void)
 {
     FSA fsa;
+    FILE *fp;
+    char tmp = 'a';
+    int i;
+    int alarm = 0;
+    
+    fp = fopen("file", "a"); //I used the mode append, but I think it's useful to create a new file for every single different AFS
+    if (fp==NULL){
+        printf("\nErrore nell'apertura del file, il file non esiste");
+        return 0;
+    }
+    printf("Inserisci gli stati dell'automa: (\invio per terminare");
+    while(tmp != '\n'){
+        do{
+            scanf("%c", &tmp);
+            fseek(fp, 0, SEEk_SET); //rewind(fp);
+            while ((!feof(fp))&&(alarm==0)){
+                
+        }while(
+        fputc(tmp, fp);
+    }
     /* Initialise FSA */
     return fsa;
 }
@@ -20,13 +40,15 @@ int main()
     while (1)
     {
         printf("Cosa vuoi fare?\n1 - Creare FSA\n2 - Utilizzare FSA\n3 - Uscire plz\n");
-        scanf("%d", &decision);
-        if (decision == 1)
+        do{
+            scanf("%d", &decision);
+        }while((decision != 1)&&(decision != 2)&&(decision != 3));
+            if (decision == 1)
             my_fsa = create_fsa();
-        else if (decision == 2)
-            compute_fsa(my_fsa);
-        else if (decision == 3)
-            break;
+            else if (decision == 2)
+                compute_fsa(my_fsa);
+            else if (decision == 3)
+                break;
     }
     return (0);
 }
